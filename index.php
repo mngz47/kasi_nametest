95adaa2476fdce442a420a1e7aaa859160356f0a
Mngz <mngz636@gmail.com>
2021-02-01 13:54:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,36 +6,6 @@
 
 </head>
 <body id=bb style="background-image:url(resource/kasi_bg.jpg);background-size:100% auto;background-position:0 0;text-align:center;padding-top:140px;" >
-
-<iframe id=four_target src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=instruments&banner=0JB0A4VDS5FZSF8J66G2&f=ifr&linkID=957b6056e810aaba8d6734b832d49fbf&t=productlists4-20&tracking_id=productlists4-20" width="468" height="60" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
-
-	<div class=forward >
-    <script>
-   
-    var affiliates = ["//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=instruments&banner=0JB0A4VDS5FZSF8J66G2&f=ifr&linkID=957b6056e810aaba8d6734b832d49fbf&t=productlists4-20&tracking_id=productlists4-20",
-		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=26&l=ur1&category=amazonhomepage&f=ifr&linkID=395a3797c0390ce90b0909dac625fbf4&t=productlists4-20&tracking_id=productlists4-20",
-		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=26&l=ur1&category=bestsellingproducts&banner=1F8VTV49P7N5K5XY5N82&f=ifr&linkID=62fbaa2c4f1eadfd296f4e00a685b4c2&t=productlists4-20&tracking_id=productlists4-20",
-		     "//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ur1&category=instruments&banner=1F5XAJEA6KXT7JPXEC02&f=ifr&linkID=e4ae1c402faaed3fc1483d4072b07ade&t=productlists4-20&tracking_id=productlists4-20",
-		     "https://www.productlists.co.za/services/banners/air_bnb.html",
-			 "https://www.productlists.co.za/services/banners/temp222.html",
-			 "https://www.productlists.co.za/services/banners/ezio.html"];
-			 
-    var index = 0;
-    
-    function four_next(){
-		if(index<affiliates.length){
-			e('four_target').src = affiliates[index];
-			index++;
-		}else{
-		index = 0;	
-		}
-	  }
-  setInterval(function(){
-  four_next();
-  },14000);
-    </script>
-</div>
-
 <script>
 
 var location_s = "resource/rr/";
